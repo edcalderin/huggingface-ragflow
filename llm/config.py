501859d789f6226019d05178a742f3b1ec9f4a98
Erick <edcm.erick@gmail.com>
2025-03-13 16:13:45 -0500

    ✨ feat: Enhance EmbeddingConfig class with model_config

--- llm/config.py
+++ llm/config.py
@@ -3,11 +3,19 @@ from pydantic_settings import BaseSettings
 
 
 class EmbeddingConfig(BaseSettings):
-    filename: str = Field(default="AI Engineer-Lead - Anexo.pdf", frozen=True)
-    embedding_model_name: str = Field(
-        default="sentence-transformers/all-mpnet-base-v2", frozen=True
+    model_config = {
+        "frozen": True,  # Makes the entire model immutable
+    }
+    filename: str = Field(
+        default="AI Engineer-Lead - Anexo.pdf", description="Document used for this RAG"
     )
-    collection_name: str = Field(default="historiacard_docs", frozen=True)
+    embedding_model_name: str = Field(default="sentence-transformers/all-mpnet-base-v2")
+    collection_name: str = Field(default="historiacard_docs")
     qdrant_store_path: str = Field(
-        default="./tmp", alias="Directory to store embeddings", frozen=True
+        default="./tmp", description="Directory to store embeddings"
     )
+
+    # Retrieval
+    model_name: str = Field(default="meta-llama/Llama-2-7b-chat-hf")
+    task: str = Field(default="text-generation")
+    temperature: float = Field(default=0.2)
